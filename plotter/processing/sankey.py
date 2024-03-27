23131be470087ec98ab0eb1ac9602c92dbcbb50b
Andreas Lubojanski <andreas@lubojanski.com>
2024-03-27 10:06:10 +0100

    added color support

--- plotter/processing/sankey.py
+++ plotter/processing/sankey.py
@@ -3,49 +3,11 @@ import os
 import pandas as pd
 import numpy as np
 import plotly.graph_objects as go
+import seaborn as sns
 
 from plotter.processing.common import (readCsvData,
                                        sumSingleColumnsFromData,
                                        reduceNodeName)
-
-
-def buildDataFrame(singleData, nodes, df):
-    link_colors = {}
-    node_colors = []
-
-    for index in singleData.index:
-        tmp_str = index.replace("(", "").replace(")", "").replace(" ", "").replace("'", "").split(",")
-
-        if len(tmp_str) < 2:
-            tmp_str.append("None")
-
-        tmp_str[0] = reduceNodeName(tmp_str[0])
-        tmp_str[1] = reduceNodeName(tmp_str[1])
-
-        if not nodes.__contains__(tmp_str[0]):
-            color = '#{:02x}{:02x}{:02x}'.format(*(np.random.choice(range(256), size=3)))
-            nodes.append(tmp_str[0])
-            link_colors[tmp_str[0]] = color
-            node_colors.append(color)
-
-        if not nodes.__contains__(tmp_str[1]):
-            color = '#{:02x}{:02x}{:02x}'.format(*(np.random.choice(range(256), size=3)))
-            nodes.append(tmp_str[1])
-            link_colors[tmp_str[1]] = color
-            node_colors.append(color)
-
-        data2append = {
-            'input': [nodes.index(tmp_str[0])],
-            'output': [nodes.index(tmp_str[1])],
-            'value': [singleData[0].loc[index]],
-            'label': [tmp_str[0] + " -> " + tmp_str[1]],
-            'color': [link_colors[tmp_str[0]]]
-        }
-
-        concat_df = pd.DataFrame(data2append)
-        df = pd.concat([df, concat_df], ignore_index=True)
-
-    return df, nodes, node_colors, link_colors
 
 
 def buildSankeyDiagram(wdir, title, output=False):
@@ -61,8 +23,45 @@ def buildSankeyDiagram(wdir, title, output=False):
 
     dataframe = pd.DataFrame(columns=["input", "output", "value", "label", "color"])
     nodelist = []
+    link_colors = {}
+    node_colors = []
 
-    dataframe, nodelist, nodecolors, linkcolors = buildDataFrame(data, nodelist, dataframe)
+    palette = sns.color_palette("Spectral", 100).as_hex()
+
+    i = 0
+    for index in data.index:
+        tmp_str = index.replace("(", "").replace(")", "").replace(" ", "").replace("'", "").split(",")
+
+        if len(tmp_str) < 2:
+            tmp_str.append("None")
+
+        tmp_str[0] = reduceNodeName(tmp_str[0])
+        tmp_str[1] = reduceNodeName(tmp_str[1])
+
+        if not nodelist.__contains__(tmp_str[0]):
+            color = str(palette[i])
+            i += 1
+            nodelist.append(tmp_str[0])
+            link_colors[tmp_str[0]] = color
+            node_colors.append(color)
+
+        if not nodelist.__contains__(tmp_str[1]):
+            color = str(palette[i])
+            i += 1
+            nodelist.append(tmp_str[1])
+            link_colors[tmp_str[1]] = color
+            node_colors.append(color)
+
+        data2append = {
+            'input': [nodelist.index(tmp_str[0])],
+            'output': [nodelist.index(tmp_str[1])],
+            'value': [data[0].loc[index]],
+            'label': [tmp_str[0] + " -> " + tmp_str[1]],
+            #'color': [link_colors[tmp_str[0]]]
+        }
+
+        concat_df = pd.DataFrame(data2append)
+        dataframe = pd.concat([dataframe, concat_df], ignore_index=True)
 
     fig = go.Figure(
         data=[go.Sankey(
@@ -74,7 +73,7 @@ def buildSankeyDiagram(wdir, title, output=False):
                 thickness=10,
                 line=dict(width=0.5),
                 label=nodelist,
-                color=nodecolors
+                color=node_colors
             ),
             # Add links
             link=dict(
@@ -82,7 +81,7 @@ def buildSankeyDiagram(wdir, title, output=False):
                 target=dataframe['output'],
                 value=dataframe['value'],
                 label=dataframe['label'],
-                color=dataframe['color']
+                #color=dataframe['color']
             )
         )]
     )
